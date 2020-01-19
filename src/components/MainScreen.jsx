3e630ea282eca520dc015f1474ca582a927eac79
Guilherme Pedro <guilhermepedro@ioasys.com.br>
2020-01-19 03:22:08 -0300

    finish poke

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,57 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import pokeLogo from '../assets/poke_logo.png';
-import { getPokemons } from '../requests/poke';
-import styled from 'styled-components';
+import React from "react";
+import pokeLogo from "../assets/poke_logo.png";
+import styled from "styled-components";
 
-const MainScreen = () => {
-  const [clicks, setClicks] = useState(0);
-  const [page, setPage] = useState(0);
-  const [pokemons, setPokemons] = useState([]);
+import Table from "./Table";
 
-  const onClickButton = () => {
-    setClicks(clicks + 1);
-  };
-
-  const callbackSucess = results => {
-    setPokemons(results);
-  };
-
-  useEffect(() => {
-    getPokemons(page, callbackSucess);
-  }, []);
-
-  return (
-    <StyledContainer>
-      <StyledLogo src={pokeLogo} />
-      <StyledLabel>Você clicou no botão {clicks} vezes</StyledLabel>
-      <StyledButton onClick={onClickButton}>Clique aqui</StyledButton>
-    </StyledContainer>
-  );
-};
+const MainScreen = () => (
+  <StyledContainer>
+    <StyledLogo src={pokeLogo} />
+    <Table />
+  </StyledContainer>
+);
 
 const StyledContainer = styled.div`
-  height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #282c34;
-`;
-
-const StyledLabel = styled.p`
-  color: white;
-  font-size: 20;
-`;
-
-const StyledButton = styled.button`
-  width: 300px;
-  height: 40px;
-  border-radius: 10px;
-  background-color: white;
-
-  :focus {
-    outline: 0;
-  }
+  padding-bottom: 40px;
+  background-color: #0d121b;
 `;
 
 const StyledLogo = styled.img`
